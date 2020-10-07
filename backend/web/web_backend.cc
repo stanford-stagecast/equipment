fd928dbeb00f300e977c3f87b898159509a4a2f5
Akshay Srivatsan <srivatsan.akshay@gmail.com>
2020-10-07 12:49:33 -0700

    feat(web-backend): websockets with threads

--- backend/web/web_backend.cc
+++ backend/web/web_backend.cc
@@ -1,19 +1,46 @@
-#include <iostream>
-#include <boost/beast.hpp>
 #include <boost/asio.hpp>
+#include <boost/asio/spawn.hpp>
+#include <boost/beast.hpp>
+#include <iostream>
+#include <string_view>
 
-namespace net = boost::asio;
 namespace beast = boost::beast;
 namespace ip = boost::asio::ip;
+namespace net = boost::asio;
+namespace websocket = beast::websocket;
+using tcp = boost::asio::ip::tcp;
 
-using namespace std;
-using namespace beast;
-using namespace beast::websocket;
+void do_session(tcp::socket socket) {
+  try {
+    websocket::stream<beast::tcp_stream> ws(std::move(socket));
+    ws.set_option(
+        websocket::stream_base::timeout::suggested(beast::role_type::server));
+    ws.set_option(
+        websocket::stream_base::decorator([](websocket::response_type &res) {
+          res.set(beast::http::field::server, std::string(BOOST_BEAST_VERSION_STRING) +
+                                           " websocket-server-coro");
+        }));
+    ws.accept();
+    ws.write(net::buffer("Hello, World!"));
+
+    for (;;) {
+      beast::flat_buffer buffer;
+      ws.read(buffer);
+
+      ws.text(ws.got_text());
+      ws.write(buffer.data());
+    }
+  } catch (beast::system_error const &se) {
+    if (se.code() != websocket::error::closed) {
+      std::cerr << "Error: " << se.code().message() << std::endl;
+    }
+  }
+}
 
 int main() {
   std::cout << "Hello, World!" << std::endl;
   net::io_context ioc;
-  tcp_stream sock(ioc);
+  beast::tcp_stream sock(ioc);
 
   ip::tcp::acceptor acceptor(ioc);
   ip::tcp::endpoint endpoint(ip::tcp::v4(), 8000);
@@ -22,23 +49,10 @@ int main() {
   acceptor.bind(endpoint);
   acceptor.listen();
 
-  while (1) {
-    stream<tcp_stream> ws(acceptor.accept());
-    ws.accept();
+  for (;;) {
+    tcp::socket socket{ioc};
+    acceptor.accept(socket);
 
-    net::const_buffer b("Hello, World!", 13);
-    ws.text(true);
-    ws.write(b);
-
-    while(1) {
-      flat_buffer buffer;
-      beast::error_code ec;
-      ws.read(buffer, ec);
-      if (ec == beast::websocket::error::closed) {
-        break;
-      }
-      ws.text(ws.got_text());
-      ws.write(buffer.data());
-    }
+    std::thread(&do_session, std::move(socket)).detach();
   }
 }
