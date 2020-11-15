0c6f976379418dd8d4d62b3b03abd48c232ec67f
George Hosono <georgehosono@gmail.com>
2020-11-15 02:26:26 -0800

    updated test

--- backend/mixer/test.cc
+++ backend/mixer/test.cc
@@ -5,6 +5,14 @@
 using namespace std;
 
 int main() {
-	Mixer mixer{{"../../tests/float_440_hz_tone.wav", "../../tests/float_441_hz_tone.wav"}};
+	cout << "This test uses relative directories and only work when run in the build directory" << endl;
+	Mixer mixer({"../tests/float_440_hz_tone.wav", "../tests/float_441_hz_tone.wav"}
+				, "left.wav", "right.wav");
+	mixer.remove_wav_headers();
+
+	while (!mixer.first_file_ended()) {
+		mixer.mix({{0, 1}, {0, 1}});
+	}
+
 	return 0;
 }
