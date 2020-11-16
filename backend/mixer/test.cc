2800914ca06f846cea27c796fc296abd29e7fd66
George Hosono <georgehosono@gmail.com>
2020-11-16 03:14:53 -0800

    rounding out test executable

--- backend/mixer/test.cc
+++ backend/mixer/test.cc
@@ -9,11 +9,16 @@ using namespace std;
 int main() {
 	cout << "This test uses relative directories and only work when run in the build directory" << endl;
 	Mixer mixer({"../tests/float_440_hz_tone.wav", "../tests/float_441_hz_tone.wav"}
-				, "left.wav", "right.wav");
+				, "left.out", "right.out");
 	mixer.remove_wav_headers();
 
 	while (!mixer.first_file_ended()) {
 		mixer.mix({{1, 1}, {-1, 1}});
+	}
+
+	Pufferizer pufferizer{"puffer_", "../tests", "left.out", "right.out"};
+	while (!pufferizer.eof()) {
+		pufferizer.pufferize_once();
 	}
 
 	return 0;
