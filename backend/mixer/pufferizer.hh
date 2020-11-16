9e35a3d4e7c23ab4d3d395f8941afe0ab3cf96b8
George Hosono <georgehosono@gmail.com>
2020-11-15 16:30:56 -0800

    Initial pufferizer header file

--- backend/mixer/pufferizer.hh
+++ backend/mixer/pufferizer.hh
@@ -1,1 +1,44 @@
+#ifndef PUFFERIZER_HH
+#define PUFFERIZER_HH
 
+#include <string>
+#include <iostream>
+#include "wav_header.hh"
+
+class Pufferizer {
+  private:
+	// The prefix of the files for it to write
+	std::string prefix_;
+
+	// The path to the files to write
+	std::string dest_;
+
+	// The location of the left channel
+	std::ifstream l_in_;
+
+	// The location of the right channel
+	std::ifstream r_in_;
+
+	// The number of the next file to write
+	size_t next_file_{0};
+
+	// This is the default header that gets written to every output file
+	WavHeader header_{
+
+	};
+
+
+  public:
+	Pufferizer(const std::string& prefix,
+		const std::string& destination,
+		const std::string& left_input,
+		const std::string& right_input);
+
+	// reads in enough data to write one 5 second wav file
+	void pufferize_once();
+
+	bool eof() const { return l_in_.eof() && r_in_.eof(); }
+};
+
+
+#endif
