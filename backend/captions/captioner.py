909f5226ad80cd6b5f75af09ca1652fcd7dc21f3
George Hosono <hosono@stanford.edu>
2021-02-25 19:24:17 -0800

    Moved captioner from C++ to Python

--- backend/captions/captioner.py
+++ backend/captions/captioner.py
@@ -1,18 +1,36 @@
 import requests
-from datetime import datetime, timezone, timedelta
-from time import sleep
+from datetime import datetime, timezone
+import json
 
-def make_one_caption(n, t):
-	now = (datetime.now(timezone.utc) + timedelta(seconds=t)).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
-	return "{} region:reg1#cue1\n<br>Caption {}<br>\n".format(now, n)
+
+class Captioner:
+
+	def __init__(self, key: str, filename: str):
+		self.url = f"http://upload.youtube.com/closedcaption?cid={key}&seq="
+		self.index = 0
+		self.captions = []
+		with open(filename, 'r') as f:
+			for line in f:
+				self.captions.append(line)
+
+	def send_next_caption(self):
+		if self.index >= len(self.captions):
+			print("No more captions to send")
+			return
+
+		now = (datetime.now(timezone.utc)).strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3]
+		caption = f"{now} region:reg1#cue1\n<br>{self.captions[self.index]}<br>\n"
+		r = requests.post(f"{self.url}{self.index}", caption, {'content-type': 'text/plain'})
+
+		self.index += 1
+		print(f"Got {r.status_code}: {r.text}")
+
 
 def main():
-	url = "http://upload.youtube.com/closedcaption?cid=vp4u-e3zq-2xzu-1wgs-azkd"
-	for i in range(10):
-		caption = make_one_caption("{}a".format(i), 0) + make_one_caption("{}b:".format(i), 1)
-		r = requests.post("{}&seq={}".format(url, 2*i+22520), caption, {'content-type': 'text/plain'})
-		print("Sent {}\nGot {}: {}".format(caption, r.status_code, r.text))
-		sleep(5)
+	with open("tokens.json", "r") as f:
+		token = json.loads(f.read())["youtube"]
+	captioner = Captioner(token, "captioner.py")
+	captioner.send_next_caption()
 
 
 if __name__ == "__main__":
